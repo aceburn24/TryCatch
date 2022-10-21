f4929f911babcaf06b9d4ef70c84c1023ad1c811
Ace Brian Nagmahal <116327276+aceburn24@users.noreply.github.com>
2022-10-21 17:26:36 +0800

    Add files via upload

---
+++ frmRegistration.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace _03Lab1
+{
+    public partial class frmRegistration : Form{
+        private string _FullName;
+        private int _Age;
+        private long _ContactNo;
+        private long _StudentNo;
+
+        /////return methods 
+        public long StudentNumber(string studNum)
+        {
+            if (Regex.IsMatch(studNum, @"^[0-9]{1,12}$"))
+            { 
+                _StudentNo = long.Parse(studNum); 
+            }
+            else
+            {
+                throw new FormatException("Wrong Input in Student Number, Please try Again");
+            }
+
+
+            return _StudentNo;
+        }
+
+        public long ContactNo(string Contact)
+        {
+            if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+            {
+                _ContactNo = long.Parse(Contact);
+            }
+            else
+            {
+                throw new FormatException("Wrong Input in Contact Number, Please try Again");
+            }
+
+            return _ContactNo;
+        }
+
+        public string FullName(string LastName, string FirstName, string MiddleInitial)
+        {
+            if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") && Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") && Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+            {
+                _FullName = LastName + ", " + FirstName + " " + MiddleInitial;
+            }
+            else
+            {
+                throw new FormatException("Wrong Input in Full Name, Please try Again");
+            }
+
+            return _FullName;
+        }
+
+        public int Age(string age)
+        {
+            if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+            {
+                _Age = Int32.Parse(age);
+            }
+            else
+            {
+                throw new FormatException("Wrong Input in Age, Please try Again");
+            }
+            return _Age;
+        }
+
+        public frmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                StudentInformationClass.SetFullName = FullName(txtLastName.Text, txtFirstName.Text, txtMiddleInitial.Text);
+                StudentInformationClass.SetStudentNo = StudentNumber(txtStudentNo.Text);
+                StudentInformationClass.SetProgram = cbPrograms.Text;
+                StudentInformationClass.SetGender = cbGender.Text;
+                StudentInformationClass.SetContactNo = ContactNo(txtContactNo.Text);
+                StudentInformationClass.SetAge = Age(txtAge.Text);
+                StudentInformationClass.SetBirthday = datePickerBirthday.Value.ToString("yyyy-MM-dd");
+                frmConfirmation frm = new frmConfirmation();
+                frm.ShowDialog();
+
+                txtLastName.Clear();
+                txtFirstName.Clear();
+                txtMiddleInitial.Clear();
+                txtContactNo.Clear();
+                txtAge.Clear();
+                txtStudentNo.Clear();
+                cbGender.SelectedIndex = -1;
+                cbPrograms.SelectedIndex = -1;
+                datePickerBirthday.Value = DateTime.Now;
+            }
+            catch
+            (FormatException Aceqtpie)
+            {
+                MessageBox.Show(Aceqtpie.Message);
+                MessageBox.Show("MALI KA");
+            }
+        }
+
+
+        private void frmRegistration_Load(object sender, EventArgs e)
+        {
+            string[] ListOfProgram = new string[]{
+                "BS Information Technology",
+                "BS Computer Science",
+                "BS Information Systems",
+                "BS in Accountancy",
+                "BS in Hospitality Management",
+                "BS in Tourism Management"
+        };
+                for (int i = 0; i < 6; i++)
+            {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            }
+        }
+    }
+}
